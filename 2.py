435c9b32f6f974a0153db45b4f8675b846abe44e
Paul E <pebre79@hotmail.com>
2014-01-28 21:46:38 -0600

    add index and 2.py

--- 2.py
+++ 2.py
@@ -1,3 +1,19 @@
+"""
+join: q = session.query(User).join(Address, User.id==Address.user_id)
+
+---
+q = session.query(Address).select_from(User).\
+                join(User.addresses).\
+                filter(User.name == 'ed')
+
+equivalent:
+SELECT address.* FROM user
+    JOIN address ON user.id=address.user_id
+    WHERE user.name = :name_1
+
+
+"""
+
 from sqlalchemy import *
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
@@ -20,6 +36,8 @@ def create():
         name = Column(String(255))
         def __init__(self, name):
             self.name = name
+        def __repr__(self):
+            return "<User(name='%s')>" % (self.name)
 
     Base.metadata.create_all(engine)
 
@@ -31,11 +49,39 @@ def ses():
         name = Column(String(255))
         def __init__(self, name):
             self.name = name
+        def __repr__(self):
+            return "<User(name='%s')>" % (self.name)
+
     Session = sessionmaker(bind=engine)
     session = Session()
     user_john = User(name='john')
     session.add(user_john)
     session.commit()
-create() # works
-ses() # works
+
+def q():
+    Base = declarative_base()
+    class User(Base):
+        __tablename__ = 'users'
+        id = Column(Integer, primary_key=True)
+        name = Column(String(255))
+        def __init__(self, name):
+            self.name = name
+        def __repr__(self):
+            return "<User(name='%s')>" % (self.name)
+ 
+    Session = sessionmaker(bind=engine)
+    session = Session()
+    #user_john = User(name='john')
+    #print user_john in session
+    #x = session.query(User).filter_by(id = 1)
+    #print x.id
+
+    #for user in session.query(User).filter(User.id==5):
+    #    print user
+    # return None if found nothing
+    print session.query(User).filter(User.id==5).first()
+    print session.query(User).get(3)
+#create() # works
+#ses() # works
 #ses()
+q()
---
+++ bot.py
@@ -0,0 +1,2 @@
+from bottle import hook, response, route, run, static_file, request
+import json
